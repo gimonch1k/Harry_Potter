b79d7b840dffa73ca05a6a80f18728530579c435
Dmytro <dimaplyakin2007@gmail.com>
2026-05-03 16:51:58 +0300

    initial commit

---
+++ src/app/App.jsx
@@ -0,0 +1,17 @@
+import { Component } from "react";
+
+import Header from "../header/Header";
+
+import "./App.scss";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default App;
